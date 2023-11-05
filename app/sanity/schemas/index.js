32c77e0501979a5571d3f86ab12b9e4181fb44d8
_krustina <kristinavannan@gmail.com>
2023-11-05 17:52:43 -0600

    latest

--- app/sanity/schemas/index.js
+++ app/sanity/schemas/index.js
@@ -1,7 +1,6 @@
-import homepage from "./homepage";
 import exhibitions from "./exhibitions";
 import info from "./info";
 import global from "./global";
 
-const schemas = [homepage, exhibitions, info, global];
+const schemas = [exhibitions, info, global];
 export default schemas;
