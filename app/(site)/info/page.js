c2865fe34f889d60393f5c18445137721b59db3d
krustina <kristina.vannan@ssense.com>
2025-06-11 22:31:04 -0400

    email

--- app/(site)/info/page.js
+++ app/(site)/info/page.js
@@ -19,7 +19,13 @@ export default async function Info() {
                     </p>
                 </div>
 
+                <div></div>
+                <a href="mailto:ddanfernan@gmail.com,nfcastonguay@gmail.com,theroux.maxime@gmail.com?subject=Ermitage%20Inquiry" target="_blank">EMAIL US</a>
+
+
                 <a href="https://www.instagram.com/ermitage_308/" target="_blank">@ermitage_308</a>
+
+
 
             </div>
         </div>
