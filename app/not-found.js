7c092884e9606642c2c909cf9426ba9b3f94ea77
krustina <kristina.vannan@ssense.com>
2026-02-25 13:06:11 -0500

    404 page edit

--- app/not-found.js
+++ app/not-found.js
@@ -20,6 +20,7 @@ export default async function NotFound() {
             <h1>404</h1>
             <br />
             <p>The page you are looking for does not exist :(</p>
+            <br />
             <a href="/">← BACK</a>
         </div>
     )
